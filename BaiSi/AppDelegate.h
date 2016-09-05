de75fdb453628eab52da4aea8c3fa0d26cee2636
小白敲代码 <982167877@qq.com>
2016-09-05 09:17:38 +0800

    Initial Commit

---
+++ BaiSi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaiSi
+//
+//  Created by stone on 16/9/5.
+//  Copyright © 2016年 zm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BaiSi/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BaiSi
+//
+//  Created by stone on 16/9/5.
+//  Copyright © 2016年 zm. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
